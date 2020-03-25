812233e8d514097aee1846c22751853c749a3dff
Aratz M. Lasa <aratzml@opendeusto.es>
2020-03-25 11:23:59 +0100

    Added main and edited run_server.sh

--- toad_influx_data/main.py
+++ toad_influx_data/main.py
@@ -1,7 +1,9 @@
+import asyncio
 from toad_influx_data.server import DataServer
 
 
-async def toad_influx_data_app():
+if __name__ == "__main__":
     data_server = DataServer()
-    await data_server.start()
-    return data_server
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(data_server.start())
+    loop.run_forever()
